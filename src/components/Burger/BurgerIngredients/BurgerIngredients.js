932629d87cdcc11e0aa79435b59e9dedb6236c47
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-09 16:22:32 -0600

    Now the burger can be rendered on the UI

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import classes from "./BurgerIngredients.css";
+import classes from "./BurgerIngredient.css";
 import PropTypes from "prop-types";
 
 class BurgerIngredients extends Component {
   render() {
     let ingredient = null;
+    console.log("hola");
     switch (this.props.type) {
       case "bread-bottom":
         ingredient = <div className={classes.BreadBottom}></div>;
+        break;
       case "bread-top":
         ingredient = (
           <div className={classes.BreadTop}>
@@ -15,16 +17,22 @@ class BurgerIngredients extends Component {
             <div className={classes.Seeds2}></div>
           </div>
         );
+        break;
       case "meat":
         ingredient = <div className={classes.Meat}></div>;
+        break;
       case "cheese":
         ingredient = <div className={classes.Cheese}></div>;
+        break;
       case "bacon":
         ingredient = <div className={classes.Bacon}></div>;
+        break;
       case "salad":
         ingredient = <div className={classes.Salad}></div>;
+        break;
       default:
         ingredient = null;
+        break;
     }
     return ingredient;
   }
