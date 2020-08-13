4e7d21b7f2cf3326334bcb7073d0f2278812efc2
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-12 19:38:01 -0600

    Added the OrderSummary component to be in charge of showing up the list of ingredients in the order, also added a general modal to show messages

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Aux from "../../../hoc/Aux";
+
+const OrderSummary = (props) => {
+  const ingredientsSummary = Object.keys(props.ingredients).map((key) => {
+    return (
+      <li key={key}>
+        <span style={{ textTransform: "capitalize" }}>{key}</span> :
+        {props.ingredients[key]}
+      </li>
+    );
+  });
+
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>A burger with:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p>Continue to checkout?</p>
+    </Aux>
+  );
+};
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import classes from "./Modal.css";
 const Modal = (props) => {
-  return <div>{props.children}</div>;
+  return <div className={classes.Modal}>{props.children}</div>;
 };
 
 export default Modal;
