8f19d627b409c77cbc9c1a3377ea32f44c38193c
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-14 04:50:09 -0600

    Added NavigationItem and NavigationItems components

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavigationItem = () => {
+  return (
+    <li>
+      {" "}
+      <a href="/">A Link</a>{" "}
+    </li>
+  );
+};
+
+export default NavigationItem;
