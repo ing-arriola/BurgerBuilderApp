10d378798eebdb5326bc061f34fd1f1618367897
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-15 07:54:28 -0600

    Added more improvements on responsive desing

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from "../../Assets/Images/burger-logo.png";
 import classses from "./Logo.css";
 const Logo = (props) => {
   return (
-    <div className={classses.Logo}>
+    <div className={classses.Logo} style={{ height: props.height }}>
       <img src={burgerLogo} alt="A burger" />
     </div>
   );
