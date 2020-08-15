e1c8324f637d3783888382e66c311fe1e82eb4a0
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-15 10:22:42 -0600

    Now the sideDRawer is hidden when the user clicks on the backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-const Layout = ({ children }) => {
-  return (
-    <Aux>
-      <SideDrawer />
-      <Toolbar />
-      <main className={classes.Content}>{children}</main>
-    </Aux>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Aux>
+        <SideDrawer
+          show={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <Toolbar />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
