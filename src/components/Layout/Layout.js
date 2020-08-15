2ba430cc9caa2ece07b94cfae3d802ea3e08ca50
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-14 19:06:29 -0600

    Added SideDrawer to the layout but... it still needs some details to get fixed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const Layout = ({ children }) => {
   return (
     <Aux>
+      <SideDrawer />
       <Toolbar />
       <main className={classes.Content}>{children}</main>
     </Aux>
