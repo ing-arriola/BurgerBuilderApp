7210579d4fe64b29c5aaa3f0e0efaff12947d6f5
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-23 15:34:09 -0600

    Added contact Data component

---
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Button from "../../../UI/Button/Button";
+
+export class ContactData extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      postalCode: "",
+    },
+  };
+  render() {
+    return (
+      <div>
+        <h4>Enter your Contact Data</h4>
+        <form>
+          <label>Your name:</label>
+          <input type="text" name="name" />
+          <label>Your email:</label>
+          <input type="email" name="email" />
+          <label>Stret:</label>
+          <input type="text" name="street" />
+          <label>Postal Code:</label>
+          <input type="text" name="pcode" />
+          <Button Btntype="Success">ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactData;
