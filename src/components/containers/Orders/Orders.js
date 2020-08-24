efcb46f4fef3d305c9d13b4b7def90c3a162e6b2
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-24 04:28:30 -0600

    Now fetching data from firebase

--- src/components/containers/Orders/Orders.js
+++ src/components/containers/Orders/Orders.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
 import Order from "../Order/Order";
+import axios from "../../../orders";
+import errorHandler from "../../../hoc/errorHandler/errorHandler";
 
 export class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((ans) => {
+        const ordersFromDb = [];
+        for (let key in ans.data) {
+          ordersFromDb.push({
+            ...ans.data[key],
+            id: key,
+          });
+        }
+        console.log(ordersFromDb);
+        this.setState({ loading: false, orders: ordersFromDb });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
